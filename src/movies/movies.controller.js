0e95fe731d8efec3091bb11b446e4de154c27619
Gabriel <wootengabriel90@gmail.com>
2022-05-18 00:20:14 -0400

    implemented migration files and started work on services/controllers/routers

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,11 +1,24 @@
-const service = require("./movies.service");
+const moviesService = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res) {
-    const data = await service.list();
-    res.json({data});
+  const showing = req.query.is_showing;
+  if (showing) {
+    const data = await moviesService.listShowing();
+    res.json({ data: data });
+  } else {
+    const data = await moviesService.list();
+    res.json({ data: data });
+  }
+}
+
+async function read(req, res) {
+  const { movieId } = req.params;
+  const data = await moviesService.read(movieId);
+  res.json({ data: data });
 }
 
 module.exports = {
-    list: [asyncErrorBoundary(list)],
-}
+  list: [asyncErrorBoundary(list)],
+  read: [asyncErrorBoundary(read)],
+};
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -5,6 +5,27 @@ async function list() {
     return result;
 }
 
+function listShowing() {
+    return knex("movies as m")
+      .select(
+        "m.movie_id",
+        "m.title",
+        "m.runtime_in_minutes",
+        "m.description",
+        "m.image_url"
+      )
+      .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+      .where({ is_showing: true })
+      .groupBy("m.movie_id");
+  }
+
+async function read(movieId) {
+    const result = knex("movies").select("*").where({movie_id: movieId}).first();
+    return result;
+}
+
 module.exports = {
     list,
+    listShowing,
+    read,
 }
