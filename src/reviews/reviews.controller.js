8eae7bf7e945ed563e81fdcee8c4d74e80f4f89a
Gabriel <wootengabriel90@gmail.com>
2022-05-24 23:23:49 -0400

    added comments to reviews functions

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,6 +1,8 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary.js");
 const reviewsService = require("./reviews.service.js");
 
+
+//reviewExists sends a read request from our service with the reviewId, if the review data exists it is stored in res.locals and used in the next function, if it is not an error is sent to our error handler.
 async function reviewExists(req, res, next) {
   const { reviewId } = req.params;
   const review = await reviewsService.read(reviewId);
@@ -12,16 +14,20 @@ async function reviewExists(req, res, next) {
   next({ status: 404, message: "Review cannot be found" });
 }
 
+
+//read displays the data of the requested reviewId, only if the reviewId passed the reviewExists check.
 function read(req, res) {
   res.json({ data: res.locals.review });
 }
 
+//destroy passes the given reviewId from request params into the delete function of reviewsService(which then removes the records related to that id from the table), then responds
 async function destroy(req, res) {
   const { reviewId } = req.params;
   await reviewsService.delete(reviewId);
   res.status(204).send("No Content");
 }
 
+//update takes the information given from our request body and creates a review object with that data nd the given review_id, then the updatedReview is passed into our service.update to update the review in the table, updatedReview is saved into locals for use in the next function to attempt to keep single responsibility;
 async function update(req, res, next) {
   const updatedReview = {
     ...req.body.data,
@@ -32,6 +38,7 @@ async function update(req, res, next) {
   next();
 }
 
+//readUpdate passes the updatedReview into showUpdate where it will respond with the updated review and altered critic keys;
 async function readUpdate(req, res) {
   const showUpdate = await reviewsService.showUpdate(res.locals.updatedReview);
   const review = showUpdate[0];
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,12 +1,14 @@
 const knex = require("../db/connection");
 const reduceProperties = require("../utils/reduce-properties");
 
+//reduce critic will alter all the properties for a critic_id in a response and add them into their own object with the key of critic
 const reduceCritic = reduceProperties("critic_id", {
   preferred_name: ["critic", null, "preferred_name"],
   surname: ["critic", null, "surname"],
   organization_name: ["critic", null, "organization_name"],
 });
 
+//read will retrieve and respond all data from the review table related to the given reviewId
 async function read(reviewId) {
   const result = await knex("reviews")
     .select("*")
@@ -15,6 +17,7 @@ async function read(reviewId) {
   return result;
 }
 
+//destroy selects all columns in the review table related to the given reviewId then deletes them from the table;
 async function destroy(reviewId) {
   const result = await knex("reviews")
     .select("*")
@@ -23,6 +26,7 @@ async function destroy(reviewId) {
   return result;
 }
 
+//showUpdate joins the critics and reviews tables and selects all columns related to the review_id within the updated review before using reduceCritic and returning the data;
 async function showUpdate(updatedReview) {
   const result = await knex("reviews as r")
     .join("critics as c", "r.critic_id", "c.critic_id")
@@ -32,6 +36,7 @@ async function showUpdate(updatedReview) {
   return result;
 }
 
+//update finds all columns related to the review_id within the updated review, then updates the with the information in updatedReview;
 async function update(updatedReview) {
   const result = await knex("reviews")
     .where({ review_id: updatedReview.review_id })
