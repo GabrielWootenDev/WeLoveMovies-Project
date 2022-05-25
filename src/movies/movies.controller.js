7b6980403ea2b403eb089d63b489a2aaff6eb455
Gabriel <wootengabriel90@gmail.com>
2022-05-24 22:56:53 -0400

    refactored showReviews function in movies.controller and added comments for files in the movies folder

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,32 +1,28 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary.js");
 const moviesService = require("./movies.service.js");
 
+//showingAtTheater takes the movieId from the request params and then calls showingAtTheater from our service with it to retrieve the list of theaters that movie is showing at, finally it returns that data as json.
 async function showingAtTheater(req, res) {
   const { movieId } = req.params;
   const theaters = await moviesService.showingAtTheater(movieId);
   res.json({ data: theaters });
 }
 
+//showReviews takes the movieId and uses the showReviews in our service with it to retrieve a list of reviews for the movies, then it alters each review to include the critic information without the critic array and only the one relevant critic and responds with our list data as json.
 async function showReviews(req, res) {
   const { movieId } = req.params;
   const reviews = await moviesService.showReviews(movieId);
-  const reviewsResponse = [];
-  reviews.map((review) => {
-    newReview = {
-      review_id: review.review_id,
-      content: review.content,
-      score: review.score,
-      created_at: review.created_at,
-      updated_at: review.updated_at,
-      critic_id: review.critic_id,
-      movie_id: review.movie_id,
+  const reviewsResponse = reviews.map((review) => {
+    review = {
+      ...review,
       critic: review.critic[0],
     };
-    reviewsResponse.push(newReview);
+    return review;
   });
   res.json({ data: reviewsResponse });
 }
 
+//movieExists sends a read request from our service with the movieId, if the movie data exists it is stored in res.locals and used in the next function, if it is not an error is sent to our error handler.
 async function movieExists(req, res, next) {
   const { movieId } = req.params;
   const data = await moviesService.read(movieId);
@@ -39,10 +35,13 @@ async function movieExists(req, res, next) {
   next({ status: 404, message: "Movie not found" });
 }
 
+
+//read displays the data of the requested movieId, only if the movie passed the movieExists check.
 async function read(req, res) {
   res.json({ data: res.locals.movie });
 }
 
+//our list function will list all movies based on one of two conditions, if the query for is_showing = true then we only call the service listShowing for movies data currently showing, if false then all movies data will be listed in json with moviesService.list();
 async function list(req, res) {
   const showing = req.query.is_showing;
   if (showing) {
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,6 +1,7 @@
 const knex = require("../db/connection");
 const reduceProperties = require("../utils/reduce-properties");
 
+//reduce critic will alter all the properties for a critic_id in a response and add them into their own object with the key of critic
 const reduceCritic = reduceProperties("critic_id", {
   critic_id: ["critic", null, "critic_id"],
   preferred_name: ["critic", null, "preferred_name"],
@@ -10,6 +11,7 @@ const reduceCritic = reduceProperties("critic_id", {
   updated_at: ["critic", null, "updated_at"],
 });
 
+//showReviews joins data from the review table and the critic table the displays all information related to the given movie_id, then alters the response to 6give all the critic information its own object with reduceCritic;
 function showReviews(movieId) {
   return knex("reviews as r")
     .join("critics as c", "r.critic_id", "c.critic_id")
@@ -18,6 +20,7 @@ function showReviews(movieId) {
     .then(reduceCritic);
 }
 
+//showingAtTheater joins the theaters and movies tables and selects all the data for the given movieId and only if it is showing at any theater
 async function showingAtTheater(movieId) {
   return await knex("theaters as t")
     .select("*")
@@ -25,6 +28,7 @@ async function showingAtTheater(movieId) {
     .where({ movie_id: movieId }, { is_showing: true });
 }
 
+//listShowing joins the movie and theaters tables and responds with all movies data currently showing at any theater;
 async function listShowing() {
   return await knex("movies as m")
     .select(
@@ -39,10 +43,12 @@ async function listShowing() {
     .groupBy("m.movie_id");
 }
 
+//read selects and responds with all the data for the given movieId
 async function read(movieId) {
   return await knex("movies").select("*").where({ movie_id: movieId }).first();
 }
 
+//list selects and responds with all of the data in the movies table;
 async function list() {
   return await knex("movies").select("*");
 }
